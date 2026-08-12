64c4e362eb49acd6e98a11c85ef7a167bfea0355
Maxwell-Bu <maxwellbulatao04@gmail.com>
2026-08-12 17:58:30 -0500

    did problems 13 and 14

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,4 +1,20 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    phrase = phrase.toUpperCase();
+    let reversed = "";
+    let filteredString = "";
+
+    for (let i = 0; i < phrase.length; i++) {
+        const letterCode = phrase.charCodeAt(i);
+        if (letterCode >= 48 && letterCode <= 57 || letterCode >= 65 && letterCode <= 90) {
+            filteredString += phrase[i];
+        }
+    }
+    
+    for (i = filteredString.length -1; i >=0; i--) {
+        reversed += filteredString[i];
+    }
+
+    return filteredString === reversed;
 
 };
 
--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(ind) {
+    let fib = [0, 1, 1];
 
+    if (ind < 0) {
+        return "OOPS";
+    } else if (ind < 3) {
+        return fib.at(ind);
+    }
+
+    for (i = 0; i < ind -2; i++) {
+        fib.push(fib.at(-1) + fib.at(-2));
+    }
+    return fib.at(-1);
 };
 
 // Do not edit below this line
