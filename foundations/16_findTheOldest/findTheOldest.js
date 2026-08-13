33f4d71933be9b437f1a1ba94ff5af1490de469a
Maxwell-Bu <maxwellbulatao04@gmail.com>
2026-08-13 16:32:59 -0500

    Finished the exercises! SO closegit add -A!

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const d = new Date();
+    const year = d.getFullYear();
+    const ages = arr.sort((a, b) => {
+        if (a.yearOfDeath === undefined) {
+            a.yearOfDeath = year;
+        } else if (b.yearOfDeath === undefined) {
+            b.yearOfDeath = year;
+        }
 
+        return (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? -1 : 1;
+    });
+
+    return ages[0];
 };
 
 // Do not edit below this line
